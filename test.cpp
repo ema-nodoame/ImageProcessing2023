9f8f29032498da75eca2cb1b721b2c318576c937
Takuma Matsuda <218266@st.takushoku-u.ac.jp>
2023-11-30 20:21:36 +0900

    第10回講義時点での進捗

--- test.cpp
+++ test.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <opencv2/opencv.hpp>
@@ -13,6 +15,53 @@ int main(int argc, char *argv[]) {
   if (img.empty()) {  // 画像ファイルが見つからない場合の処理
     printf("Input image is not found.\n");
     return EXIT_FAILURE;
+  }
+
+  auto clip = [](int v) {
+    // 0~255の範囲に画素値を収めるためのクリッピング処理を行うラムダ式
+    if (v > 255) {
+      v = 255;
+    }
+    if (v < 0) {
+      v = 0;
+    }
+    return v;
+  };
+
+  auto gamma_correcution = [](int v, double gamma) {
+    // ガンマ補正を行うラムダ式
+    double in = static_cast<double>(v) / 255.0;
+    double out = pow(in, gamma) * 255.0;
+    return static_cast<int>(out);
+  };
+
+  auto quantization = [](int v, double delta) {
+    double x = static_cast<double>(v);
+    double Qx = floor(x / delta + 0.5);
+    return Qx;
+  };
+
+  auto dequantization = [](double v, double delta) {
+    double rec = v * delta;
+    return static_cast<int>(rec);
+  };
+
+  const int W = img.cols;
+  const int H = img.rows;
+  uint8_t *pixels = img.data;
+  const double delta = 255.0;
+  // Stride access
+  for (int y = 0; y < H; ++y) {
+    for (int x = 0; x < W; ++x) {
+      // ストライドアクセスによるメモリ上のアドレスの計算
+      int idx = y * W + x;
+      // 画素値の取得
+      int val = pixels[idx];
+      // 処理
+      val *= dequantization(quantization(val, delta), delta);
+      // 処理結果の画素値をメモリに書き込む
+      pixels[idx] = clip(val);
+    }
   }
 
   // 画像の表示
